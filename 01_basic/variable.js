6472ce4307a12019d5a33fa79814307c3694ce0e
Ishtika Barode <113988424+ishtika@users.noreply.github.com>
2024-07-28 12:51:40 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountid =12345 // it will not change even if we try to change this
+let email = "ishhu@google.com"
+var pass = "256"
+/*
+we are not using var becuase of it's scope isssue 
+*/
+accountcity = "jaipur"
+//change
+email = "yess@gmail.com"
+pass = " 4895"
+accountcity= "bhopal"
+//print
+console.table([email, pass, accountcity])
+
+// refer 2 documentation 1 mdm  2nd tc39
