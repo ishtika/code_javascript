8283d0c38bc91af0546e6749a08cd9e83b98516f
Ishtika Barode <113988424+ishtika@users.noreply.github.com>
2024-07-29 06:06:43 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,8 @@
+//primitive
+//7 category 
+//primitive daatype are call by value - basically iska reference ni diya jata inko memory me se copy karke dia jata hai
+//and changes are done in copy
+//string, number, boolean, null , undefined, symbool, bigint
+
+//second type reference type (non primitive)
+//arrays,objects,functions
